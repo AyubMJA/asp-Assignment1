1938e0e48e35ce48b5a0a876be1855ae8ca32ec8
Ayub Ali <48361671+TheOnlyAyub@users.noreply.github.com>
2021-09-28 14:14:58 -0400

    Add project files.

---
+++ assignment1/Models/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace assignment1.Models
+{
+    public class Car
+    {
+        [Key]
+        public int ID;
+
+        [Required]
+        public string Make;
+
+        [Required]
+        public string Model;
+
+        [Required]
+        public string Colour;
+
+        public int Year;
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Purchase")]
+
+        public DateTime? PurchaseDate { get; set; }
+
+        [Range(0, 999999)]
+        public int Kilometers { get; set; }
+    }
+}
